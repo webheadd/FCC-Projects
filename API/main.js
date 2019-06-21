7820a8947c6ed0354abe467f2ea12f8dcd990069
webheadd <rodolfo_stamaria_2008@yahoo.com>
2019-06-21 10:30:53 +0800

    v.1.3
    Added clickable card and modal

--- API/main.js
+++ API/main.js
@@ -1,16 +1,21 @@
 var searchBox = document.querySelector("#search");
 var ul = document.querySelector("#demo");
-var paginate = document.querySelector(".paginate");
+var paginate = document.querySelector(".pagination");
+var prevBtn = document.getElementById("left");
+var nextBtn = document.getElementById("right");
+var modalContainer = document.querySelector(".modal");
+var container = document.querySelector(".container");
+var closeModal = document.querySelector("#closeModal");
 
 var arrayText = [];
 var html = "";
 var li = document.getElementsByClassName("list-item");
 var listTitle = document.getElementsByClassName("title");
-var page = 1;
+var modal = "";
 
 //configs
 var api_key = "e0a2e02905a8a197ce79913b80c65105";
-//var url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page}&region=us`;
+
 var url = "https://api.themoviedb.org/3/";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -18,108 +23,135 @@ document.addEventListener("DOMContentLoaded", () => {
   load(uri);
 });
 
+//next page
+nextBtn.addEventListener("click", function() {
+  let page = document.getElementById("page").value;
+  page++;
+  document.getElementById("page").value = page;
+  checkIf(page);
+});
+//previous page
+prevBtn.addEventListener("click", function() {
+  let page = document.getElementById("page").value;
+  page--;
+
+  if (page < 1) page = 1;
+  document.getElementById("page").value = page;
+  checkIf(page);
+});
+//check pagination
+function checkIf(page) {
+  var searched = searchBox.value;
+  var imageUrl;
+  if (searched === "") {
+    imageUrl = `${url}movie/popular?api_key=${api_key}&language=en-US&page=${page}&region=us`;
+  } else {
+    imageUrl = `${url}search/movie?api_key=${api_key}&language=en-US&query=${searched}&page=${page}&include_adult=false`;
+  }
+  html = "";
+  load(imageUrl);
+  console.log(page);
+}
+//movie details
+
 //load request
-var load = (queryUrl, isSearched) => {
+var load = queryUrl => {
   var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var text = JSON.parse(xhr.responseText);
       arrayText.push(text.results);
-      if (!isSearched) console.log("no search");
-      if (isSearched) console.log("searched");
+
       text.results.forEach(result => {
-        creatItems(result.poster_path, result.vote_average);
+        creatItems(result.poster_path, result.vote_average, result.id);
+      });
+      Array.from(li).forEach(list => {
+        list.addEventListener("click", function() {
+          container.style.transform = "scale(1)";
+          var movieID = this.getAttribute("data-attribute");
+          var detailsUrl = `${url}movie/${movieID}?api_key=${api_key}&language=en-US`;
+
+          loadDetails(detailsUrl);
+        });
       });
     }
   };
   xhr.open("GET", queryUrl, true);
   xhr.send();
 };
+//Close Modal
+closeModal.addEventListener("click", () => {
+  container.style.transform = "scale(0)";
+});
+
+//get movie infos
+var loadDetails = queryUrl => {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var text = JSON.parse(xhr.responseText);
+      console.log(text.genres);
+
+      var movieGenres = text.genres;
+      var genreNames = movieGenres
+        .map(genre => genre.name)
+        .sort()
+        .join(", ");
+      modal = "";
+      createModal(
+        text.poster_path,
+        text.overview,
+        text.title,
+        text.runtime,
+        genreNames
+      );
+    }
+  };
+  xhr.open("GET", queryUrl, true);
+  xhr.send();
+};
+//creater modal details
+var createModal = (backdrop, overview, title, runtime, genre) => {
+  var backDropImg = `https://image.tmdb.org/t/p/original${backdrop}`;
+  modal += `<img src=${backDropImg} alt="image" class="modal_poster" />
+            <ul class="modal_details">
+              <li class="modal_li modal_header">${title}</li>
+              <li class="modal_li"><strong>Genre</strong>: ${genre}</li>
+              <li class="modal_li"><strong>Runtime</strong>: ${runtime} minutes</li>
+              <li class="modal_li"><strong>Summary</strong>: ${overview}</li>
+            </ul>`;
+
+  modalContainer.innerHTML = modal;
+};
 
 searchBox.addEventListener("keyup", searchMovie);
 //search function
+var toUrl;
 function searchMovie(e) {
-  const text = e.target.value;
-  let toUrl;
+  var text = e.target.value;
+
   if (text === "") {
     toUrl = `${url}movie/popular?api_key=${api_key}&language=en-US&page=1&region=us`;
+    document.getElementById("page").value = 1;
   } else {
-    toUrl = `${url}movie/popular?api_key=${api_key}&language=en-US&query=${text}&page=1&include_adult=false`;
+    toUrl = `${url}search/movie?api_key=${api_key}&language=en-US&query=${text}&page=1&include_adult=false`;
+    console.log(toUrl);
+    document.getElementById("page").value = 1;
   }
-
-  load(toUrl, true);
-  // for (let i = 0; i < arrayText[0].length; i++) {
-  //   console.log(arrayText[0][i].title.toLowerCase());
-  //   if (arrayText[0][i].title.toLowerCase().indexOf(text) != -1) {
-  //     li[i].style.display = "block";
-  //     setTimeout(() => {
-  //       li[i].style.opacity = "1";
-  //     }, 150);
-  //   } else {
-  //     li[i].style.opacity = "0";
-  //     setTimeout(() => {
-  //       li[i].style.display = "none";
-  //     }, 150);
-  //   }
-  // }
+  html = "";
+  arrayText = [];
+  load(toUrl);
 }
 
 //create HTML items
-var creatItems = (poster, voteCount) => {
-  var imageUrl = `https://image.tmdb.org/t/p/original${poster}`;
-
-  html += `<li class="list-item">
-            <img src="${imageUrl}" alt="image" class="poster">
+var creatItems = (poster, voteCount, id) => {
+  var imagePoster = `https://image.tmdb.org/t/p/original${poster}`;
+  html += `<li class="list-item" data-attribute="${id}">
+            <img src="${imagePoster}" alt="image" class="poster">
            <p class="vote">${voteCount}</p>
            </li>`;
+
   ul.innerHTML = html;
 };
-
-//create pagination
-var pageItems = "";
-var pages = document.getElementsByClassName("pages");
-
-var createPages = num => {
-  pageItems += `<list class="pages">${num}</list>`;
-  paginate.innerHTML = pageItems;
-};
-// create a page of 10
-for (let i = 1; i <= 10; i++) {
-  createPages(i);
-}
-// click event in pages
-Array.from(pages).forEach(num => {
-  num.addEventListener("click", changePage);
-});
-
-function changePage() {
-  page = this.innerHTML;
-  html = "";
-  arrayText = [];
-  load();
-}
-
-var action = document.querySelector("#action");
-
-action.addEventListener("click", showAction);
-function showAction() {
-  for (let i = 0; i < arrayText[0].length; i++) {
-    var x = [];
-    x.push(arrayText[0][i]);
-
-    x.forEach(num => {
-      if (num.genre_ids.includes(28)) {
-        li[i].style.display = "block";
-        setTimeout(() => {
-          li[i].style.opacity = "1";
-        }, 150);
-      } else {
-        li[i].style.opacity = "0";
-        setTimeout(() => {
-          li[i].style.display = "none";
-        }, 150);
-      }
-    });
-  }
-}
